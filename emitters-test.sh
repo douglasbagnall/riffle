842191b20a424230291c1b39b0f197b3011b1cc6
Douglas Bagnall <douglas@paradise.net.nz>
2011-04-26 00:02:53 +1200

    emitters-test output goes to subdirectory

--- emitters-test.sh
+++ emitters-test.sh
@@ -1,8 +1,10 @@
 #/bin/bash
+DEST=test-results
+mkdir -p $DEST
 
 for f in $@; do
     echo $f
-    results=$(basename $f)-test.txt
+    results=$DEST/$(basename $f)-test.txt
     $f | dieharder -g 200 -s 1 > $results
     $f | dieharder -g 200 -r 4 -d 1 -p 25 >> $results
     $f | dieharder -g 200 -r 2 -d 2 >> $results
