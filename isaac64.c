728f961dc2fdc4f207404e1e66b6ce7dbccee1d9
Douglas Bagnall <douglas@paradise.net.nz>
2011-04-30 22:43:35 +1200

    use shared init code for isaac64.c

--- isaac64.c
+++ isaac64.c
@@ -12,6 +12,12 @@
 #include <time.h>               /* for seeding to current time */
 #include "random_helpers.h"
 #include "ccan/isaac/isaac64.h"
+#ifndef KEY_BYTES
+#define KEY_BYTES (160 / 8)
+#endif
+#ifndef IV_BYTES
+#define IV_BYTES (0 / 8)
+#endif
 
 #define MODULE_NAME isaac64
 
@@ -46,10 +52,6 @@ random_random(RandomObject *self)
 #endif
 }
 
-/*
- * The rest is Python-specific code, neither part of, nor derived from, the
- * Twister download.
- */
 
 static PyObject *
 random_seed(RandomObject *self, PyObject *args)
@@ -59,35 +61,18 @@ random_seed(RandomObject *self, PyObject *args)
     if (!PyArg_UnpackTuple(args, "seed", 0, 1, &arg))
         return NULL;
 
-    u8 seed[20] = "12345678901234567890";
-    if (arg == NULL || arg == Py_None) {
-	/*XXX should use urandom */
-        time_t now;
-        time(&now);
-	snprintf((char *)seed, sizeof(seed), "%lx%p%p", now, &now, &self);
-    }
-    else if (PyObject_CheckReadBuffer(arg)){
-	const void *buffer;
-	Py_ssize_t buffer_len;
-	if (PyObject_AsReadBuffer(arg, &buffer, &buffer_len)){
-	    return NULL;
-	}
-	initialise_state(seed, sizeof(seed), (u8*)buffer, buffer_len);
-    }
-    else {
-	/*use python hash. it would be possible, but perhaps surprising to
-	  use the string representation */
-	long hash = PyObject_Hash(arg);
-	//debug("seeding with hash %ld\n", hash);
-	snprintf((char *)seed, sizeof(seed), "%ld", hash);
-    }
-    /*now we have a seed */
+    u8 seed[KEY_BYTES + IV_BYTES];
+    memset(seed, '#', sizeof(seed));
 
+    if (extract_seed(arg, seed, sizeof(seed)) != 0){
+	return NULL;
+    }
     isaac64_init(&self->context, seed, sizeof(seed));
-
     Py_INCREF(Py_None);
     return Py_None;
 }
+
+
 
 static PyObject *
 random_getstate(RandomObject *self)
